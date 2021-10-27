399f2753d50c392826721244dda629e6e608d4c4
6alex6kash6 <6alex6kash6@gmail.com>
2021-10-27 21:51:10 +0300

    feat: add cors support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("./db/mongoose");
 const { User, Task } = require("./models");
 const { userRouter, taskRouter } = require("./routers");
@@ -7,6 +8,7 @@ const { userRouter, taskRouter } = require("./routers");
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 
 app.use(userRouter);
 app.use(taskRouter);
